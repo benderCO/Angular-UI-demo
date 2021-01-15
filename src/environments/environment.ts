e3ef63bdaab3207c8f525e1f13cf78a688cd302a
Mike Wester <mpwester@gmail.com>
2021-01-14 20:08:05 -0600

    Basic Chart Implmentation

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,11 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+import{ IEnvironment } from './environment.interface';
 
-export const environment = {
-  production: false
+export const environment: IEnvironment = {
+  production: false,
+  baseUrl: 'https://demo.liquidware.com/'
 };
 
 /*
