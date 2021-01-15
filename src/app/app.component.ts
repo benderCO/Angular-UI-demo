fae8c30f6b9c34aabeb7c79bf0502274df836992
Mike Wester <mpwester@gmail.com>
2021-01-14 20:35:08 -0600

    Successfully showing two charts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { ChartOptions } from './interface/chart-options';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-ui-demo';
+    public title: string = 'angular-ui-demo';
+    public chartOptions1: ChartOptions = { name: 'name1' };
+    public chartOptions2: ChartOptions = { name: 'name2' };
 }
---
+++ src/app/interface/chart-options.ts
@@ -0,0 +1,3 @@
+export interface ChartOptions {
+    name: string
+}
