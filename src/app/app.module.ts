4e0b17f2d49e59665adb5641c0bca3d3b7da2c0d
Mike Wester <mpwester@gmail.com>
2021-01-15 23:11:10 -0600

    Add API calls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ChartComponent } from './chart/chart.component';
-import { MainComponent } from './main/main.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { ChartComponent } from './components/chart/chart.component';
+import { MainComponent } from './core/main/main.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
     declarations: [
